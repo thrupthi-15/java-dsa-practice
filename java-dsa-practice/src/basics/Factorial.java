d682a96351a6bf83f415ec6372df023342c7b33a
thrupthi-15 <thrupthigowda81@gmail.com>
2026-05-08 23:29:42 +0530

    Add Factorial program to calculate the factorial of a number

---
+++ java-dsa-practice/src/basics/Factorial.java
@@ -0,0 +1,17 @@
+package basics;
+
+import java.util.*;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.err.println(fact);
+        sc.close();
+    }
+}
