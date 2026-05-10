8ce33285924da4781fc14b44cfc7bb4f93caf7ee
thrupthi-15 <thrupthigowda81@gmail.com>
2026-05-10 22:35:52 +0530

    Add GCD program to calculate the greatest common divisor of two numbers

---
+++ java-dsa-practice/src/basics/GcdOfTwoNo.java
@@ -0,0 +1,19 @@
+package basics;
+
+import java.util.*;
+
+public class GcdOfTwoNo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println(a);
+        sc.close();
+    }
+}
