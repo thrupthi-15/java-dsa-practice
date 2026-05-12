30c78c2fd5260009b4184060546e51b9c6905ce6
thrupthi-15 <thrupthigowda81@gmail.com>
2026-05-12 14:49:56 +0530

    Add DefaultConstructor class with Student instantiation and default constructor

---
+++ java-dsa-practice/src/oops/Constructor/DefaultConstructor.java
@@ -0,0 +1,17 @@
+package oops.Constructor;
+
+class Student {
+
+    Student() {
+        System.out.println("Default Student Constructor");
+    }
+
+}
+
+public class DefaultConstructor {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+    }
+}
